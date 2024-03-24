ce4b95b4d42ecf4df5720d54bf73c4ea9cc2997c
Khaif Mohamed <khaifmohamededava@gmail.com>
2024-03-24 09:18:11 +0530

    minor changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,3 +23,5 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+module.exports = app;
--- server/src/routes/Route.ts
+++ server/src/routes/Route.ts
@@ -1,13 +1,9 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import { getAllSoftwares, sendMail } from "../controllers/controller";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("testing.....");
-});
-
-router.get('/softwares',getAllSoftwares)
+router.get("/softwares", getAllSoftwares);
 router.post("/send-mail", sendMail);
 
 export default router;
