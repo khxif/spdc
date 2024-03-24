fba0595e9d7f677ce62e11524058fc00256d17bf
Khaif Mohamed <khaifmohamededava@gmail.com>
2024-03-24 09:23:30 +0530

    added a test route

--- server/src/routes/Route.ts
+++ server/src/routes/Route.ts
@@ -3,6 +3,7 @@ import { getAllSoftwares, sendMail } from "../controllers/controller";
 
 const router = Router();
 
+router.get("/", (req, res) => res.send("hi"));
 router.get("/softwares", getAllSoftwares);
 router.post("/send-mail", sendMail);
 
