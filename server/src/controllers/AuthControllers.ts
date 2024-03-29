e30b283babd76533c48d2f012b30b4f42a804630
Khaif Mohamed <khaifmohamededava@gmail.com>
2024-03-29 22:41:29 +0530

    cookie option changes

--- server/src/controllers/AuthControllers.ts
+++ server/src/controllers/AuthControllers.ts
@@ -60,7 +60,7 @@ export const login = async (req: Request, res: Response) => {
     const token = createToken(user._id, user.username, user.email);
     console.log(token);
 
-    res.cookie("user", token, { maxAge: 30 * 24 * 60 * 60, path: "/" });
+    res.cookie("user", token, cookieOptions);
     // res.setHeader("Set-Cookie", `user=${token}; Path=/`);
 
     res.status(200).json({
--- server/src/utils/cookieOptions.ts
+++ server/src/utils/cookieOptions.ts
@@ -4,10 +4,11 @@ const expiryDate = new Date();
 expiryDate.setDate(expiryDate.getDate() + 10);
 
 export const cookieOptions: CookieOptions = {
+  maxAge: 30 * 24 * 60 * 60,
   path: "/",
-  expires: expiryDate,
+  // expires: expiryDate,
   secure: false,
   sameSite: "none",
-  // domain:
-  //   process.env.NODE_ENV === "production" ? "spdc-api.vercel.app" : "localhost",
+  domain:
+    process.env.NODE_ENV === "production" ? "spdc-api.vercel.app" : "localhost",
 };
