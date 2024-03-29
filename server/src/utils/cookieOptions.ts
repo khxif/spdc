34acdfccdea1aa780f1ac310dd5cdde7f8f3c75a
Khaif Mohamed <khaifmohamededava@gmail.com>
2024-03-29 22:43:16 +0530

    cookie option changes

--- server/src/utils/cookieOptions.ts
+++ server/src/utils/cookieOptions.ts
@@ -4,11 +4,11 @@ const expiryDate = new Date();
 expiryDate.setDate(expiryDate.getDate() + 10);
 
 export const cookieOptions: CookieOptions = {
-  maxAge: 30 * 24 * 60 * 60,
+  // maxAge: 30 * 24 * 60 * 60,
   path: "/",
-  // expires: expiryDate,
-  secure: false,
-  sameSite: "none",
-  domain:
-    process.env.NODE_ENV === "production" ? "spdc-api.vercel.app" : "localhost",
+  expires: expiryDate,
+  // secure: false,
+  // sameSite: "none",
+  // domain:
+  //   process.env.NODE_ENV === "production" ? "spdc-api.vercel.app" : "localhost",
 };
