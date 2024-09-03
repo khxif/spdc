c02fc11af78d1d9a68366df09132e6d8cbb882f6
Khaif Mohamed <khaifmohamededava@gmail.com>
2024-09-03 19:29:05 +0530

    removed teams section

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -2,7 +2,6 @@ import Contact from "@/sections/Home/Contact";
 import HomeHero1 from "@/sections/Home/HomeHero1";
 import HomeHero2 from "@/sections/Home/HomeHero2";
 import Products from "@/sections/Home/Products";
-import Team from "@/sections/Home/Team";
 
 export default async function HomePage() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/v1/softwares`, {
@@ -15,7 +14,6 @@ export default async function HomePage() {
       <HomeHero1 />
       <HomeHero2 />
       <Products softwares={softwares} />
-      <Team />
       <Contact />
     </main>
   );
